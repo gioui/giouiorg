19670f2e3e9d235d411c6b08ff13da6587c31982
Elias Naur <mail@eliasnaur.com>
2019-12-02 15:59:37 +0100

    cmd/giouiorg: implement snippet addressing with regular expressions
    Similar to how the present tool can cut out a snippet from a larger
    program text, include markdown can now specify a range on the form:
    
    	<{{example/blank_test.go}}[/START OMIT/,/END OMIT/]
    
    Signed-off-by: Elias Naur <mail@eliasnaur.com>

--- cmd/giouiorg/page.go
+++ cmd/giouiorg/page.go
@@ -3,6 +3,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"errors"
 	"fmt"
@@ -13,6 +14,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/gomarkdown/markdown"
@@ -103,7 +105,7 @@ func loadMarkdown(url string) ([]byte, error) {
 	}
 	mdp := parser.NewWithExtensions(parser.CommonExtensions | parser.Includes | parser.Attributes)
 	mdp.Opts.ReadIncludeFn = func(from, path string, addr []byte) []byte {
-		content, err := includeExample(path, addr)
+		content, err := includeExample(path, string(addr))
 		if err != nil {
 			content = []byte(err.Error())
 		}
@@ -121,9 +123,76 @@ func loadMarkdown(url string) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func includeExample(path string, addr []byte) ([]byte, error) {
+func includeExample(path string, addr string) ([]byte, error) {
 	path = filepath.Join(includeRoot, path)
-	return ioutil.ReadFile(path)
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+	if addr != "" {
+		rng := strings.SplitN(addr, ",", 2)
+		if len(rng) != 2 {
+			return nil, fmt.Errorf("invalid address: %s", addr)
+		}
+		start, end := rng[0], rng[1]
+		startR, err := regexFor(start)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %v", start, err)
+		}
+		endR, err := regexFor(end)
+		if err != nil {
+			return nil, fmt.Errorf("%s: %v", end, err)
+		}
+		startIdx := startR.FindIndex(content)
+		if startIdx != nil {
+			content = content[startIdx[0]:]
+		}
+		endIdx := endR.FindIndex(content)
+		if endIdx != nil {
+			content = content[:endIdx[1]]
+		}
+		_ = endR
+	}
+	content = bytes.Trim(content, "\n\r")
+	content = append(content, '\n')
+	return undent(content), err
+}
+
+// undent removes the number of leading tab characters in the first
+// line from all lines.
+func undent(text []byte) []byte {
+	first := true
+	ntabs := 0
+	var buf bytes.Buffer
+	scanner := bufio.NewScanner(bytes.NewReader(text))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "OMIT") {
+			continue
+		}
+		if first {
+			for ntabs < len(line) && line[ntabs] == '\t' {
+				ntabs++
+			}
+			first = false
+		}
+		i := 0
+		for i < ntabs && len(line) > 0 && line[0] == '\t' {
+			i++
+			line = line[1:]
+		}
+		buf.WriteString(line)
+		buf.WriteByte('\n')
+	}
+	return buf.Bytes()
+}
+
+func regexFor(r string) (*regexp.Regexp, error) {
+	if len(r) < 2 || r[0] != '/' || r[len(r)-1] != '/' {
+		return nil, errors.New("missing / separators")
+	}
+	r = r[1 : len(r)-1]
+	return regexp.Compile(r)
 }
 
 func loadPage(content []byte) (page, error) {
